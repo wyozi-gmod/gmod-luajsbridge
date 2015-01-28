49f0788319b5db75b629e58723ba910a76ecdedf
wyozi <joonasmertanen@gmail.com>
2015-01-28 14:50:07 +0200

    Fix indentation

--- luajsbridge.lua
+++ luajsbridge.lua
@@ -37,7 +37,7 @@ function luajsbridge.Meta:__index(key)
 		return rawget(self, key)
 	end
 
-    luajsbridge.Debug("Creating a SubBridge for ", key, " in ", table.concat(self._pathComps, "."))
+	luajsbridge.Debug("Creating a SubBridge for ", key, " in ", table.concat(self._pathComps, "."))
 
 	local subBridge = luajsbridge.CreateBridge(self._pnl)
 	table.Add(subBridge._pathComps, self._pathComps)
@@ -58,46 +58,46 @@ function luajsbridge.Meta:__call(...)
 end
 
 hook.Add("InitPostEntity", "LJSBridge_OverrideDHTMLInit", function()
-    -- Override DHTML:Init() to automatically create a Bridge per DHTML component
-    local tbl = vgui.GetControlTable("DHTML")
+	-- Override DHTML:Init() to automatically create a Bridge per DHTML component
+	local tbl = vgui.GetControlTable("DHTML")
 
-    local oldinit = tbl.OldInit or tbl.Init
-    tbl.OldInit = oldinit
-    function tbl:Init()
-    	oldinit(self)
+	local oldinit = tbl.OldInit or tbl.Init
+	tbl.OldInit = oldinit
+	function tbl:Init()
+		oldinit(self)
 
-    	self.Bridge = luajsbridge.CreateBridge(self)
-    end
+		self.Bridge = luajsbridge.CreateBridge(self)
+	end
 end)
 
 concommand.Add("ljsbridge_test", function()
-    local dhtml = vgui.Create("DHTML")
-    dhtml:SetHTML([[
-        <script>
-        test = {}
-        test.inner = {}
-        test.inner.func = function(str, num, obj) {
-        	console.log("str: " + str);
-        	console.log("num: " + num);
-        	console.log("obj.str: " + obj.str);
-        	console.log("obj.num: " + obj.num);
-        	console.log("obj.innerTable.key: " + obj.innerTable.key);
-        }
-        </script>
-    ]])
+	local dhtml = vgui.Create("DHTML")
+	dhtml:SetHTML([[
+		<script>
+		test = {}
+		test.inner = {}
+		test.inner.func = function(str, num, obj) {
+			console.log("str: " + str);
+			console.log("num: " + num);
+			console.log("obj.str: " + obj.str);
+			console.log("obj.num: " + obj.num);
+			console.log("obj.innerTable.key: " + obj.innerTable.key);
+		}
+		</script>
+	]])
 
-    local bridge = dhtml.Bridge
+	local bridge = dhtml.Bridge
 
-    bridge.test.inner.func("Hello JS", 42, {
-    	str = "string",
-    	num = 13,
-    	innerTable = {
-            key = "value"
-        }
-    })
+	bridge.test.inner.func("Hello JS", 42, {
+		str = "string",
+		num = 13,
+		innerTable = {
+			key = "value"
+		}
+	})
 
-    -- Need to give some time for JS to run
-    timer.Simple(2, function()
-        dhtml:Remove()
-    end)
+	-- Need to give some time for JS to run
+	timer.Simple(2, function()
+		dhtml:Remove()
+	end)
 end)
